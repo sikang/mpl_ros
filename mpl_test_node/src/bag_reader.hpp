720f42497dbcb776feb22c06ef075b4ec6ecdf09
sikang <lskwdlskwd@gmail.com>
2017-12-27 21:32:05 -0500

    not sure why the bag_reader doesnt work

--- mpl_test_node/src/bag_reader.hpp
+++ mpl_test_node/src/bag_reader.hpp
@@ -1,11 +1,12 @@
-#include <ros/ros.h>
+#include <ros/console.h>
 #include <rosbag/bag.h>
 #include <rosbag/view.h>
 
 #include <boost/foreach.hpp>
 
+// num indicates the max number of elements to read, -1 means read till the end
 template<class T>
-T read_bag(std::string file_name, std::string topic) {
+std::vector<T> read_bag(std::string file_name, std::string topic, int num) {
   rosbag::Bag bag;
   bag.open(file_name, rosbag::bagmode::Read);
 
@@ -14,18 +15,18 @@ T read_bag(std::string file_name, std::string topic) {
 
   rosbag::View view(bag, rosbag::TopicQuery(topics));
 
-  bool find = false;
-  T msg;
+  std::vector<T> msgs;
   BOOST_FOREACH (rosbag::MessageInstance const m, view) {
     if (m.instantiate<T>() != NULL) {
-      msg = *m.instantiate<T>();
-      ROS_WARN("Get data!");
-      find = true;
-      break;
+      msgs.push_back(*m.instantiate<T>());
+      if(msgs.size() > num)
+        break;
     }
   }
   bag.close();
-  if (!find)
+  if (msgs.empty())
     ROS_WARN("Fail to find '%s' in '%s'", topic.c_str(), file_name.c_str());
-  return msg;
+  else
+    ROS_INFO("Get data!");
+  return msgs;
 }
