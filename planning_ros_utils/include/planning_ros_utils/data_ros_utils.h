0e1373ad0c334bf43150b077e769b2baa487bfbf
sikang <lskwdlskwd@gmail.com>
2018-08-17 10:53:01 -0400

    add template in data_ros_utils.h

--- planning_ros_utils/include/planning_ros_utils/data_ros_utils.h
+++ planning_ros_utils/include/planning_ros_utils/data_ros_utils.h
@@ -25,14 +25,14 @@ inline Aff3f toTF(const geometry_msgs::Pose &p) {
 }
 
 template <int Dim>
-sensor_msgs::PointCloud vec_to_cloud(const vec_Vecf<Dim> &pts) {
+sensor_msgs::PointCloud vec_to_cloud(const vec_Vecf<Dim> &pts, decimal_t h = 0) {
   sensor_msgs::PointCloud cloud;
   cloud.points.resize(pts.size());
 
   for (unsigned int i = 0; i < pts.size(); i++) {
     cloud.points[i].x = pts[i](0);
     cloud.points[i].y = pts[i](1);
-    cloud.points[i].z = Dim == 2 ? 0 : pts[i](2);
+    cloud.points[i].z = Dim == 2 ? h : pts[i](2);
   }
   return cloud;
 }
@@ -57,13 +57,13 @@ inline vec_Vec3f ros_to_path(const planning_ros_msgs::Path &msg) {
 }
 
 template <int Dim>
-planning_ros_msgs::Path path_to_ros(const vec_Vecf<Dim> &path) {
+planning_ros_msgs::Path path_to_ros(const vec_Vecf<Dim> &path, decimal_t h = 0) {
   planning_ros_msgs::Path msg;
   for (const auto &itt : path) {
     geometry_msgs::Point pt;
     pt.x = itt(0);
     pt.y = itt(1);
-    pt.z = Dim == 2 ? 0 : itt(2);
+    pt.z = Dim == 2 ? h : itt(2);
     msg.waypoints.push_back(pt);
   }
   return msg;
@@ -71,19 +71,19 @@ planning_ros_msgs::Path path_to_ros(const vec_Vecf<Dim> &path) {
 
 template <int Dim>
 planning_ros_msgs::PathArray
-path_array_to_ros(const vec_E<vec_Vecf<Dim>> &paths) {
+path_array_to_ros(const vec_E<vec_Vecf<Dim>> &paths, decimal_t h = 0) {
   planning_ros_msgs::PathArray msg;
   for (const auto &it : paths)
-    msg.paths.push_back(path_to_ros(it));
+    msg.paths.push_back(path_to_ros(it, h));
   return msg;
 }
 
 template <int Dim>
 planning_ros_msgs::PathArray path_array_to_ros(
-    const std::vector<std::pair<std::string, vec_Vecf<Dim>>> &paths) {
+    const std::vector<std::pair<std::string, vec_Vecf<Dim>>> &paths, decimal_t h = 0) {
   planning_ros_msgs::PathArray msg;
   for (const auto &it : paths) {
-    planning_ros_msgs::Path path_msg = path_to_ros(it.second);
+    planning_ros_msgs::Path path_msg = path_to_ros(it.second, h);
     path_msg.name = it.first;
     msg.paths.push_back(path_msg);
   }
